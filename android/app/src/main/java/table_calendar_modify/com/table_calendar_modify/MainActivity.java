4a7f7a295b21b561ae168069039fb6227d554d42
Hussein <hussienalisy997test@gmile.com>
2023-06-12 16:08:34 +0300

    mark dates , the marks look like events

---
+++ android/app/src/main/java/table_calendar_modify/com/table_calendar_modify/MainActivity.java
@@ -0,0 +1,6 @@
+package table_calendar_modify.com.table_calendar_modify;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Table Calendar Demo',
+      home: CalendarPage(),
+    );
+  }
+}
+
+class CalendarPage extends StatefulWidget {
+  @override
+  _CalendarPageState createState() => _CalendarPageState();
+}
+
+class _CalendarPageState extends State<CalendarPage> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  List<DateTime> _markedDates = [
+    DateTime(2023, 6, 1),
+    DateTime(2023, 6, 5),
+    DateTime(2023, 6, 10),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Table Calendar Demo'),
+      ),
+      body: Column(
+        children: [
+          TableCalendar(
+            focusedDay: _focusedDay,
+            firstDay: DateTime(2021),
+            lastDay: DateTime(2025),
+            calendarFormat: _calendarFormat,
+            onFormatChanged: (format) {
+              setState(() {
+                _calendarFormat = format;
+              });
+            },
+            selectedDayPredicate: (day) {
+              return _selectedDay == day;
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay;
+              });
+            },
+            eventLoader: _getEvents,
+            calendarStyle: CalendarStyle(
+              selectedDecoration: BoxDecoration(
+                color: Colors.red,
+                shape: BoxShape.rectangle,
+              ),
+              markerDecoration: BoxDecoration(
+                color: Colors.red,
+                shape: BoxShape.rectangle,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Text('Selected Day: ${_selectedDay.toString()}'),
+        ],
+      ),
+    );
+  }
+
+  List<DateTime> _getEvents(DateTime day) {
+    if (_markedDates.contains(day)) {
+      return [day];
+    } else {
+      return _markedDates.where((date) => date.day == day.day && date.month == day.month && date.year == day.year).toList();
+    }
+  }
+}
